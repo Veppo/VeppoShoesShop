aa6526995e9b33e691452328a437fec2b6e336e2
Joao Veppo <joao.veppo@valtech.com>
2020-03-06 16:54:19 -0300

    Adding immer for array manipulation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import { MdAddShoppingCart } from 'react-icons/md';
 import api from '../../services/api';
 import { formatPrice } from '../../util/format';
 
 import { ProductList } from './styles';
 
-export default class Home extends Component {
+class Home extends Component {
   state = {
     products: [],
   };
@@ -21,6 +22,15 @@ async componentDidMount() {
   this.setState({ products: data });
 }
 
+handleAddProduct = product => {
+  const { dispatch } = this.props;
+
+  dispatch({
+    type: 'ADD_TO_CART',
+    product,
+  });
+}
+
   render() {
     const { products } = this.state;
     return (
@@ -33,7 +43,9 @@ async componentDidMount() {
             <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
 
-            <button type="button">
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product)}>
               <div>
                 <MdAddShoppingCart size={16} color="#FFF"/> 3
               </div>
@@ -46,3 +58,5 @@ async componentDidMount() {
     );
   }
 }
+
+export default connect()(Home);
